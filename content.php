30a5583f3c2be1e59448d25daa1a525db50f7d54
Cryse Hillmes <tyk5555@hotmail.com>
2017-08-28 17:08:07 +0800

    将界面几处应该改为中文的地方改为中文。

--- content.php
+++ content.php
@@ -23,11 +23,11 @@ $listlayout = (empty($listlayout)) ? 'new_layout' : $listlayout;
 			</span>
 			<span class="visible-lg visible-md visible-sm pull-left">
 			<?php $category = get_the_category(); echo '<a href="' . get_category_link($category[0] -> term_id) . '"><i class="fa fa-folder-open-o"></i> ' . $category[0] -> cat_name . '</a>'; ?>
-			<a href="<?php the_permalink() ?>#respond"><i class="fa fa-commenting-o"></i> <?php echo kratos_comments_users($post->ID); ?> Comments</a>
+			<a href="<?php the_permalink() ?>#respond"><i class="fa fa-commenting-o"></i> <?php echo kratos_comments_users($post->ID); ?> 条评论</a>
 			</span>
 			<span class="pull-left">
-			<a href="<?php the_permalink() ?>"><i class="fa fa-eye"></i> <?php echo kratos_get_post_views(); ?> Views</a>
-			<a href="<?php the_permalink() ?>"><i class="fa fa-thumbs-o-up"></i> <?php if( get_post_meta($post->ID,'love',true) ){ echo get_post_meta($post->ID,'love',true); } else { echo '0'; }?> Times</a>
+			<a href="<?php the_permalink() ?>"><i class="fa fa-eye"></i> <?php echo kratos_get_post_views(); ?> 次浏览</a>
+			<a href="<?php the_permalink() ?>"><i class="fa fa-thumbs-o-up"></i> <?php if( get_post_meta($post->ID,'love',true) ){ echo get_post_meta($post->ID,'love',true); } else { echo '0'; }?> 次点赞</a>
 			</span>
 		</div>
 	</header>
